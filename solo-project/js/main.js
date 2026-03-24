00f00d674a37d6d544925d74a8cbb5e807b6d788
Mike <mike.luong06@gmail.com>
2026-03-25 00:35:15 +0200

    Solo project html/css and added javascript to get data from API.

---
+++ solo-project/js/main.js
@@ -0,0 +1,30 @@
+const url = "https://media2.edu.metropolia.fi/restaurant/api/v1/restaurants";
+
+async function loadRestaurants() {
+  const response = await fetch(url);
+  const data = await response.json();
+
+  console.log(data); // see structure
+  renderRestaurants(data);
+}
+
+loadRestaurants();
+
+function renderRestaurants(restaurants) {
+  const container = document.getElementById("restaurantList");
+  container.innerHTML = "";
+
+  restaurants.forEach(r => {
+    const card = document.createElement("div");
+    card.className = "restaurant-card";
+
+    card.innerHTML = `
+      <h3>${r.name}</h3>
+      <p>Address: ${r.address}</p>
+      <p>City: ${r.city}</p>
+      <p>Company: ${r.company}</p>
+    `;
+
+    container.appendChild(card);
+  });
+}
