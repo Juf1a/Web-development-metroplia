4109c886ae6ecf163d3240bf67fa65521787a356
Mike <mike.luong06@gmail.com>
2026-04-04 00:47:25 +0300

    Assignment 5 done swapped from dummy data to using mysql.

--- Week_3/src/app.js
+++ Week_3/src/app.js
@@ -1,14 +1,19 @@
 import express from 'express';
 import catRouter from './api/routes/catRoutes.js';
 import userRouter from './api/routes/userRoutes.js';
+import 'dotenv/config';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/public', express.static('public'));
-app.use('/api/v1/cats', catRouter);
-app.use('/api/v1/users', userRouter)
 
+app.use('/api/v1/cats', catRouter);
+app.use('/api/v1/users', userRouter);
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
 
 export default app;
